862fe42db21f01edc293d5162fe2535026ab8029
Alexander Vashurin <alexandervashurin@yandex.ru>
2017-01-23 13:50:41 +0500

    Update Booking.java

--- app/models/Booking.java
+++ app/models/Booking.java
@@ -4,12 +4,10 @@ import play.data.validation.MaxSize;
 import play.db.jpa.Model;
 
 import javax.persistence.Entity;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 
 /**
- * Created by zaicys on 21.01.2017.
+ * Created by Вашурин Александр on 21.01.2017.
  */
 @Entity
 public class Booking extends Model {
