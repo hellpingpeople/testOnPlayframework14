ed855a4609942714df445df498ce3668c92de234
Alexander Vashurin <alexandervashurin@yandex.ru>
2017-01-23 13:49:34 +0500

    Update Bootstrap.java

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -1,19 +1,35 @@
-import play.test.*;
-import play.jobs.*;
-import models.*;
+import models.Persona;
+import play.db.DB;
+import play.jobs.Job;
+import play.jobs.OnApplicationStart;
+import play.test.Fixtures;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 @OnApplicationStart
 public class Bootstrap extends Job {
-    
+
+
     public void doJob() throws SQLException {
-        // Load default data if the database is empty
+        // Запускается проверка заполнены ли необходимые таблицы,
+        //ежели нет - они заполняются
         if(Persona.count() == 0) {
+
             Fixtures.load("data.yml");
+            Connection conn = DB.getConnection();
+            conn.createStatement().execute("update persona set auto_id=13 where id=5;\n" +
+                    "update persona set auto_id=14 where id=6;\n" +
+                    "update persona set auto_id=15 where id=3;\n" +
+                    "update persona set auto_id=16 where id=10;\n" +
+                    "update persona set auto_id=17 where id=11;\n" +
+                    "update persona set auto_id=18 where id=12;\n" +
+                    "update persona set auto_id=19 where id=7;\n" +
+                    "update persona set auto_id=19 where id=4;\n" +
+                    "update persona set auto_id=19 where id=8;\n" +
+                    "update persona set auto_id=19 where id=9;\n" +
+                    "update persona set auto_id=19 where id=13;");
+
         }
     }
     
